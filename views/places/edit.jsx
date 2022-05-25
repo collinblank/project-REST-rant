fedd0904de556d9560655bbccfc741f46a908ee5
cblank <cblank@classicalconversations.com>
2022-05-25 12:34:50 -0400

    updated stars, deleting places, and updating

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -6,7 +6,7 @@ function edit_form (data) {
         <Def>
         <main>
             <h1>Edit Place</h1>
-        <form method="POST" action={`/places/${data.id}?_method=PUT`}>
+        <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
          <div className="row">
             <div className="form-group col-sm-6">
                 <label htmlFor="name">Place Name</label>
@@ -29,7 +29,17 @@ function edit_form (data) {
                 <label htmlFor="cuisines">Cuisines</label>
                 <input className="form-control" id="cuisines" name="cuisines" defaultValue={data.place.cuisines} required />
             </div>
-                <input className="btn btn-primary" type="submit" value="Edit Place"/>
+            <div>
+                <div className="form-ground col-sm-4">
+                    <label htmlFor="founded">Founded</label>
+                    <input className="form-control"
+                    id="founded"
+                    name="founded"
+                    value={data.place.founded}
+                    />
+                </div>
+            </div>
+                <input className="btn btn-primary" type="submit" value="Update Place"/>
         </form>
 
         </main>
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,7 +7,25 @@ function show(data) {
             No comments yet!
         </h3>
     )
+    let rating = (
+        <h3 className="inactive">
+            Not yet rated
+        </h3>
+    )
     if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = Math.round(sumRatings / data.place.comments.length)
+        let stars = ''
+        for (let i = 0; i < averageRating; i++) {
+            stars += '⭐️'
+        } 
+        rating = (
+            <h3>
+                {stars} stars
+            </h3>
+        )
         comments = data.place.comments.map(c => {
             return (
                 <div className="border">
@@ -33,7 +51,7 @@ function show(data) {
                 <div className="col-sm-6">
                     <h1>{data.place.name}</h1>
                 <h2>Rating</h2>
-                <p>Not Rated</p>
+                <p>{rating}</p>
                 <h2>Description</h2>
                     <h3>{data.place.showEstablished()}</h3>
                     <h4>Serving {data.place.cuisines}</h4>
@@ -44,7 +62,7 @@ function show(data) {
                 <a href={`/places/${data.id}/edit`} className="btn btn-warning">
                     Edit
                 </a>
-                <form method="POST" action={`/places/${data.id}?_method=DELETE`}  >
+                <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}  >
                     <button type="submit" className="btn btn-danger">
                         Delete
                     </button>
