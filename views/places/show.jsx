67e834d6774ef1cfee3afae31e5f423069ae2bd5
cblank <cblank@classicalconversations.com>
2022-05-25 12:17:06 -0400

    Added Comment ability

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,12 +1,30 @@
 const React = require('react')
 const Def = require('../default')
 
-function show() {
+function show(data) {
+    let comments = (
+        <h3 className="inactive">
+            No comments yet!
+        </h3>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+            return (
+                <div className="border">
+                    <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <strong>- {c.author}</strong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                </div>
+            )
+        })
+    }
     return (
         <Def>
             <main>
                 <h1>Add a New Place</h1>
-                {message}
                <div className="row">
                    <div className="col-sm-6">
                 <img src={data.place.pic} alt={data.place.name} width="400px"/>
@@ -20,6 +38,7 @@ function show() {
                     <h3>{data.place.showEstablished()}</h3>
                     <h4>Serving {data.place.cuisines}</h4>
                 <h2>Comments</h2>
+                {comments}
                 <p>No Comments yet!</p>
                 </div>
                 <a href={`/places/${data.id}/edit`} className="btn btn-warning">
@@ -31,6 +50,32 @@ function show() {
                     </button>
                 </form>
                 </div>
+                <hr/>
+                <h2>Got Your Own Rant or Rave?</h2>
+                <form action={`/places/${data.place.id}/comment`} method="POST">
+                    <div className="row">
+                        <div className="form-group col-sm-4">
+                            <label htmlFor="author">Author</label>
+                            <input id="author" name="author" className="form-control" />
+                        </div>
+                        <div className="form-group col-sm-4">
+                            <label htmlFor="stars">Star Rating</label>
+                            <input type="range" step="0.5" min="1" max="5" id="stars" name="stars" className="form-control-range" />
+                        </div>
+                        <div className="form-group col-sm-2">
+                            <label htmlFor="rant">Rant?</label>
+                            <input type="checkbox" id="rant" name="rant" className="form-check-input" />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="form-group col-sm-12">
+                            <label htmlFor="content">Content</label>
+                            <textarea id="content" name="content" className="form-control"></textarea>
+                        </div>
+                    </div>
+                    <br></br>
+                    <input type="submit" className="btn btn-primary" value="Add Comment" />
+                </form>
             </main>
         </Def>
     )
